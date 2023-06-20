bf868aeb5b01315b10568c2cefe238eb92de4eef
jto-d <jodonnell072@gmail.com>
2023-06-20 15:02:07 -0400

    Initial Commit

---
+++ flock/Core/Onboarding/View/TitleView.swift
@@ -0,0 +1,20 @@
+//
+//  TitleView.swift
+//  flock
+//
+//  Created by Jack O'Donnell on 6/20/23.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView()
+    }
+}
---
+++ flock/flockApp.swift
@@ -0,0 +1,17 @@
+//
+//  flockApp.swift
+//  flock
+//
+//  Created by Jack O'Donnell on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct flockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
