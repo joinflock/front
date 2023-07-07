4f3e78ac7a514c890474854a13e648fe60a8e308
James Zhang <james.zhang@princeton.edu>
2023-07-07 09:32:39 -0400

    modeldata now encodable

--- flock/Model/ModelData.swift
+++ flock/Model/ModelData.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-final class ModelData: ObservableObject {
+final class ModelData: ObservableObject, Encodable {
     @Published var profile = Profile(firstName: "", lastName: "", phoneNum: "", countryCode: "", birthday: Date(),
                                      university: "", languages_known: "", hometown: "", gender: "", ethnicity: "", affiniites: "")
     
