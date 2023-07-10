f3a260dc130df72f7fc1dcf25e455fb9b07beff9
Jack <90474780+jto-d@users.noreply.github.com>
2023-07-10 14:17:49 -0400

    implementing nav using NavigationStack

--- flock/Core/Auth/Views/OnboardingView.swift
+++ flock/Core/Auth/Views/OnboardingView.swift
@@ -86,9 +86,9 @@ struct OnboardingView: View {
         .onChange(of: manager.active) { newValue in
             showPrevBtn = newValue != OnboardingManager.Screen.allCases.first
         }
-        .ignoresSafeArea()
+//        .ignoresSafeArea()
         .alert(isPresented: $manager.hasError, error: manager.error) {}
-        .navigationBarBackButtonHidden(true)
+        .navigationBarBackButtonHidden()
         
     }
 }
