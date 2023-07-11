b7bfd8a7daa3fa604dc0c45f3d557b48bdd4c0f5
Saarthak Chaturvedi <sc5133@princeton.edu>
2023-07-11 16:42:18 -0400

    basic messages page

---
+++ flock/Core/Auth/Views/Messages/MessagesView.swift
@@ -0,0 +1,61 @@
+//
+//  MessagesView.swift
+//  flock
+//
+//  Created by Saarthak Chaturvedi on 7/11/23.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack(spacing: 16) {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 24, weight: .heavy))
+                    Text("Saarthak's Flocks")
+                        .font(.system(size: 24, weight: .bold))
+                    Spacer()
+                    Image(systemName: "gear")
+                }
+                .padding()
+                
+                ScrollView {
+                    ForEach(0..<10, id: \.self) {num in
+                        VStack {
+                            HStack(spacing: 16) {
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 32))
+                                    .padding(8)
+                                    .overlay(RoundedRectangle(cornerRadius: 44)
+                                        .stroke(Color(.label), lineWidth: 1))
+                                    
+                                VStack(alignment: .leading) {
+                                    Text("User Name")
+                                        .font(.system(size: 16, weight: .bold))
+                                    Text("Latest Message")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.secondary)
+                                }
+                                Spacer()
+                                Text("2d")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            Divider()
+                                .padding(.vertical, 8)
+                        } .padding(.horizontal)
+                    }
+                }
+            }.navigationBarHidden(true)
+        }
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+        MessagesView()
+            .preferredColorScheme(.dark)
+    }
+}
